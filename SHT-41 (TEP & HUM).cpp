95d9b7afbd6fdd97d48ab930aac7bb4fdd57e1d6
Harshad Chavan <101922712+Harshad31@users.noreply.github.com>
2024-03-14 11:24:08 +0530

    Update SHT-41 (TEP & HUM).cpp

--- SHT-41 (TEP & HUM).cpp
+++ SHT-41 (TEP & HUM).cpp
@@ -1,26 +1,32 @@
-#include <Wire.h>   // Include the Wire library for I2C communication
-#include <SGP40.h>  // Include the SGP40 library for SGP40 sensor
+#include <Wire.h>  // Include the Wire library for I2C communication
+#include <SHT4X.h> // Include the SHT4X library for SHT41 sensor
 
-SGP40 sgp40;
+SHT4X sht41;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin(); // Initialize I2C bus
-  sgp40.begin(0x44); // Initialize SGP40 sensor
+  sht41.begin(0x44); // Initialize SHT41 sensor
 }
 
 void loop() {
-  // Read VOC index from the SGP40 sensor
-  uint16_t VOC_index = sgp40.getVOCIndex();
+  // Read temperature and humidity from the SHT41 sensor
+  float temperature = sht41.readTemperature();
+  float humidity = sht41.readHumidity();
 
   // Check if reading is successful
-  if (VOC_index != SGP40_ERR_INVALID_VALUE) {
-    Serial.print("VOC Index: ");
-    Serial.println(VOC_index);
+  if (!isnan(temperature) && !isnan(humidity)) {
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" °C");
+
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
 
     Serial.println();
   } else {
-    Serial.println("Failed to read from SGP40 sensor!");
+    Serial.println("Failed to read from SHT41 sensor!");
   }
 
   delay(2000); // Delay between readings
